2f2e7e04ce07f010eb75c72a65df06c2e81fe0df
丞禹 <syf373586@alibaba-inc.com>
2025-07-11 11:13:08 +0800

    sunyf combine test for ds on ppu. support correct response for multiple function call  @20250708-1524 \n associate with this pr. https://github.com/sgl-project/sglang/pull/6655 in this version: 0.4.6.post1

--- python/sglang/srt/function_call_parser.py
+++ python/sglang/srt/function_call_parser.py
@@ -26,7 +26,7 @@ except Exception as e:
     )
 
 logger = logging.getLogger(__name__)
-logger.warning("""sunyf combine test for ds on ppu. support correct response for multiple function call  @20250708-1524 \n
+logger.debug("""sunyf combine test for ds on ppu. support correct response for multiple function call  @20250708-1524 \n
                associate with this pr. https://github.com/sgl-project/sglang/pull/6655 in this version: 0.4.6.post1"""
                )
 
